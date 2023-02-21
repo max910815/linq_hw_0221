075638c4059f6cd70493db16f2d76d3b671423f8
lipohan <a0966474739@gmail.com>
2023-02-21 10:49:09 +0800

    加入專案檔案。

---
+++ linq_hw_oddeven/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linq_hw_oddeven
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>()
+            {
+                8,7,9,3,11,6,2,18
+            };
+            var odd = list.Where(x => x % 2 == 0);
+            var even = list.Where(x =>x % 2 == 1);
+
+            Console.Write("odd number :");
+            foreach (var x in odd)
+            {
+                Console.Write($" {x},");
+            }
+            Console.Write("\neven number :");
+            foreach(var x in even)
+            {
+                Console.Write($" {x},");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ linq_hw_order/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linq_hw_order
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>()
+            {
+                12,18,13,23,32
+            };
+
+            var order = list.OrderByDescending(x => x).ToList();
+            Console.Write("OrderByDescending :");
+            foreach (var item in order)
+            {
+                Console.Write($" {item},");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
